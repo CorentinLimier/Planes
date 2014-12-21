d535a6c4f91f4accc41af52f2daa436d16ce76d5
Cocoloco <cocoloco@PC-Corentin-DEBIAN>
2014-12-21 07:25:09 +0100

    update crash

--- game/bo/motherFuckingBullet.py
+++ game/bo/motherFuckingBullet.py
@@ -13,14 +13,14 @@ class MotherFuckingBullet():
     def update(self):
         self.move_forward()
     
-    def move_forward(self, frameCount = 1):
+    def move_forward(self, frame_count = 1):
         angle = math.radians(self.angle)
-        self.position[0] += self.speed * frameCount * math.cos(angle)
-        self.position[1] -= self.speed * frameCount * math.sin(angle)
-        self.isCrashed()
+        self.position[0] += self.speed * frame_count * math.cos(angle)
+        self.position[1] -= self.speed * frame_count * math.sin(angle)
+        self.update_crashed()
 
         
-    def isCrashed(self):
+    def update_crashed(self):
         if  self.position[0] > self.width or self.position[1] > self.height or self.position[0] < 0 or self.position[1] < 0:
             self.crashed = True
         if  self.position > (self.width, self.height) or self.position > (self.width, self.height):
